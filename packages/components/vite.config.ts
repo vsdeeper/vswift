5afb16a309a3b79faf1f945a22caeb349f9579df
vsdeeper <vsdeeper@qq.com>
2024-04-29 18:39:51 +0800

    chore: complete config

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
@@ -27,6 +28,9 @@ export default defineConfig({
           importStyle: 'sass'
         })
       ]
+    }),
+    ElementPlus({
+      ignoreComponents: ['AutoResizer'] // 解决 Table-v2 AutoResizer with manual import: failed to resolve css file
     })
   ],
   resolve: {
