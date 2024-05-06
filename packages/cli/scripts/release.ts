6e827c71db150fd931e809a41d41b80219cd8183
vsdeeper <vsdeeper@qq.com>
2024-05-06 18:54:42 +0800

    perf: Optimize build

--- packages/cli/scripts/release.ts
+++ packages/cli/scripts/release.ts
@@ -14,7 +14,7 @@ export async function release(options: CommandOptions) {
 
   let releasePackageJson: Record<string, any> | undefined
   try {
-    if (pkg === 'utils') {
+    if (['utils', 'components'].includes(pkg)) {
       releasePackageJson = toReleasePackageJson(pkg)
     }
     if (pkg === 'cli') {
@@ -44,36 +44,68 @@ export async function release(options: CommandOptions) {
 
 function toReleasePackageJson(pkgName: PkgName) {
   try {
-    const pkgPath = path.resolve(process.cwd(), `packages/${pkgName}/package.json`)
-    const storePackage = readFileSync(pkgPath)
-    const parsePackage = JSON.parse(readFileSync(pkgPath).toString())
-    parsePackage.main = 'dist/index.cjs'
-    parsePackage.module = 'dist/index.js'
-    parsePackage.types = 'dist/types/index.d.ts'
-    const matchFiles = globSync(path.resolve(process.cwd(), 'packages/utils/core/*.ts'))
-    const exportsFiles = matchFiles.map((filePath) =>
-      filePath.split(`/packages/${pkgName}/core/`)[1].replace('.ts', '')
-    )
-    parsePackage.exports = {}
-    for (const fileName of exportsFiles) {
-      if (fileName === 'index') {
-        parsePackage.exports['.'] = {
-          import: `./dist/${fileName}.js`,
-          require: `./dist/${fileName}.cjs`,
-          types: `./dist/types/${fileName}.d.ts`
+    if (pkgName === 'utils') {
+      const pkgPath = path.resolve(process.cwd(), `packages/${pkgName}/package.json`)
+      const storePackage = readFileSync(pkgPath)
+      const parsePackage = JSON.parse(readFileSync(pkgPath).toString())
+      parsePackage.main = 'dist/index.cjs'
+      parsePackage.module = 'dist/index.js'
+      parsePackage.types = 'dist/types/index.d.ts'
+      const matchFiles = globSync(path.resolve(process.cwd(), 'packages/utils/core/*.ts'))
+      const exportsFiles = matchFiles.map((filePath) =>
+        filePath.split(`/packages/${pkgName}/core/`)[1].replace('.ts', '')
+      )
+      parsePackage.exports = {}
+      for (const fileName of exportsFiles) {
+        if (fileName === 'index') {
+          parsePackage.exports['.'] = {
+            import: `./dist/${fileName}.js`,
+            require: `./dist/${fileName}.cjs`,
+            types: `./dist/types/${fileName}.d.ts`
+          }
+        } else {
+          parsePackage.exports[`./${camel(fileName)}`] = {
+            import: `./dist/${fileName}.js`,
+            require: `./dist/${fileName}.cjs`,
+            types: `./dist/types/${fileName}.d.ts`
+          }
         }
-      } else {
+      }
+      writeFileSync(pkgPath, JSON.stringify(parsePackage, null, 2))
+      return {
+        revert: () => {
+          writeFileSync(pkgPath, storePackage)
+        }
+      }
+    } else if (pkgName === 'components') {
+      const pkgPath = path.resolve(process.cwd(), `packages/${pkgName}/package.json`)
+      const storePackage = readFileSync(pkgPath)
+      const parsePackage = JSON.parse(readFileSync(pkgPath).toString())
+      const matchFiles = globSync(
+        path.resolve(process.cwd(), 'packages/components/src/components/my-*/*.ts')
+      )
+      const exportsFiles = matchFiles.map(
+        (filePath) => filePath.split('/index.ts')[0].split('src/components/')[1]
+      )
+      parsePackage.exports = {
+        '.': {
+          import: './dist/index.js',
+          require: './dist/index.cjs',
+          types: './dist/types/index.d.ts'
+        }
+      }
+      for (const fileName of exportsFiles) {
         parsePackage.exports[`./${camel(fileName)}`] = {
           import: `./dist/${fileName}.js`,
           require: `./dist/${fileName}.cjs`,
-          types: `./dist/types/${fileName}.d.ts`
+          types: `./dist/types/${fileName}/index.d.ts`
         }
       }
-    }
-    writeFileSync(pkgPath, JSON.stringify(parsePackage, null, 2))
-    return {
-      revert: () => {
-        writeFileSync(pkgPath, storePackage)
+      writeFileSync(pkgPath, JSON.stringify(parsePackage, null, 2))
+      return {
+        revert: () => {
+          writeFileSync(pkgPath, storePackage)
+        }
       }
     }
   } catch (error) {
--- packages/components/src/App.vue
+++ packages/components/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { getLabelByValue } from "@vswift/utils";
 </script>
 
 <template>
-  {{ getLabelByValue(1, [{ label: "333", value: 1 }]) }}
   <el-tabs tab-position="left">
     <el-tab-pane label="User">User</el-tab-pane>
     <el-tab-pane label="Config">Config</el-tab-pane>
