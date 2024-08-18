3556e6338fda36f43b0bd24cef5ddf57ca567853
vsdeeper <vsdeeper@qq.com>
2024-08-18 10:37:07 +0800

    feat: delete redundant files

--- templates/vue-admin/src/router/index.ts
+++ templates/vue-admin/src/router/index.ts
@@ -22,16 +22,7 @@ const router = createRouter({
       name: 'MainLayout',
       redirect: '/dashboard',
       component: MainLayout,
-      children: [
-        {
-          path: '/about',
-          name: 'about',
-          // route level code-splitting
-          // this generates a separate chunk (About.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import('../views/AboutView.vue')
-        }
-      ]
+      children: []
     },
     {
       path: '/login',
