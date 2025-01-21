d1aaf4c237d61682d434ba6899361eb8ea6d5d8f
vsdeeper <vsdeeper@qq.com>
2025-01-21 13:58:06 +0800

    fix: fix type problem

--- templates/vue-admin/src/utils/constants.ts
+++ templates/vue-admin/src/utils/constants.ts
@@ -3,8 +3,8 @@ export const APP_SETTING_STORAGE_KEY = 'app_setting'
 // token存储key
 export const TOKEN_STORAGE_KEY = 'token'
 // 员工状态
-export const EMPLOYEE_STATUS_OPTIONS: OptionItem[] = [
+export const EMPLOYEE_STATUS_OPTIONS = [
   { label: '在职', value: 1 },
   { label: '离职', value: 2 },
-  { label: '停职', value: 3 }
+  { label: '停职', value: 3 },
 ]
